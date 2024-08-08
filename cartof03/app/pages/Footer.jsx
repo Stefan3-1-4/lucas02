fc046c753e2cf5a0c9e570c8a9dcd0db5bfb3e39
Stefan3-1-4 <stefancoadamusic@gmail.com>
2024-08-09 01:00:14 +0300

    navabr+futerIcons
    added social media icons stuff to nav and footer

--- cartof03/app/pages/Footer.jsx
+++ cartof03/app/pages/Footer.jsx
@@ -6,11 +6,11 @@ export default function thisisyesyesyesfooter() {
         <section className=''>
             <div className='flex justify-between rounded-full bg-zinc-900 p-7'>
                 <strong className='text-zinc-600'>Follow me</strong>
-                <span>
-                    <a href="github">imgGit</a>
-                    <a href="linkedin">imgIN</a>
-                    <a href="medium">imgMed</a>
-                    <a href="figma">imgFig</a>
+                <span className='flex space-x-7'>
+                  <a href=""> <img src="/images/gitPurpleNoBg.png" className='w-7 h-7' /></a>
+                  <a href=""> <img src="/images/linkedIn2n.png" className='w-7 h-7' /></a>
+                  <a href=""> <img src="/images/figma-removebg.png.png" className='w-7 h-7 rounded-full' /></a>
+                  <a href=""> <img src="/images/mediun.jpg" className='w-7 h-7 rounded-full' /></a>
                 </span>
             </div>
         </section>
--- cartof03/app/pages/Navbar.jsx
+++ cartof03/app/pages/Navbar.jsx
@@ -4,11 +4,17 @@ import Image from 'next/image'
 
 export default function Navbar() {
   return (
-    <div className='sm:fixed sm:justify-center sm:bottom-28 sm:left-1/2 sm:transform -translate-x-1/2 lg:fixed lg:top-0 lg:mt-14'>
+    <div className='z-10 sm:fixed sm:justify-center sm:bottom-28 sm:left-1/2 sm:transform -translate-x-1/2 lg:fixed lg:top-0 lg:mt-14'>
       <div className='sm: flex justify-between min-w-max p-6 bg-zinc-900 rounded-full'> 
           <Link href="/" className='px-4 text-violet-400'>About Me</Link>
           <Link href="/experience" className='px-4' >Experience</Link>
           <Link href="/futuregoals" className='px-4'>Future Goals</Link>
+          <span className='flex space-x-3'>
+            <a href=""> <img src="/images/gitPurpleNoBg.png" className='w-7 h-7' /></a>
+            <a href=""> <img src="/images/linkedIn2n.png" className='w-7 h-7' /></a>
+            <a href=""> <img src="/images/figma-removebg.png.png" className='w-7 h-7 rounded-full' /></a>
+            <a href=""> <img src="/images/mediun.jpg" className='w-7 h-7 rounded-full' /></a>
+          </span>
       </div>
     </div>
   )
