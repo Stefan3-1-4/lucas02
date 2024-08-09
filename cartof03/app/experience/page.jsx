60a39c9b1ac08d29510f17b4979629c89796d17f
Stefan3-1-4 <stefancoadamusic@gmail.com>
2024-08-09 03:10:15 +0300

    2xl styles added for experience page
    added styles for the 2xl

--- cartof03/app/experience/page.jsx
+++ cartof03/app/experience/page.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 export default function Experience() {
   return (
     <main className='text-zinc-600'>
-      <header className='xs:flex-col xs:place-content-center sm:mt-28 sm:space-y-14'>
-        <p className='text-zinc-400 sm:text-lg sm:leading-7 sm:text-center sm:mx-48'>&lt; Get to know me better and explore my journey as a <span className='text-white font-bold'>Front-End Software Engineer</span> /&gt;</p>
+      <header className='xl:mt-48 xl:text-xl xs:flex-col xs:place-content-center sm:mt-28 sm:space-y-14'>
+        <p className='text-zinc-400 xl:mx-96 xl:text-2xl sm:text-lg sm:leading-7 sm:text-center sm:mx-48'>&lt; Get to know me better and explore my journey as a <span className='text-white font-bold'>Front-End Software Engineer</span> /&gt;</p>
         <a className='sm:flex sm:text-center sm:place-content-center sm:w-fit sm:mx-auto sm:p-2 sm:px-7 sm:font-bold sm:border-2 sm:rounded-full sm:bg-white'>DownloadCv</a>
       </header>
-      <section className='bg-zinc-900 rounded-2xl sm:flex-col sm:space-y-14 sm:mt-28 sm:mx-14 sm:leading-7 sm:py-14 sm:px-7'>
-        <div className=''>
+      <section className='bg-zinc-900 rounded-2xl xl:text-2xl xl:flex xl:flex-row sm:flex-col sm:space-y-14 sm:mt-28 sm:mx-14 sm:leading-7 sm:py-14 sm:px-7'>
+        <div className='xl:min-w-96 xl:mt-14 xl:flex-col'>
           <h2 className='text-white font-bold'>Front end Sowftare engineer</h2>
           <span className='text-violet-400 sm:my-96'>Contact</span>
           <p>time</p>
           <p>location</p>
         </div>
-        <div className='sm:leading-10'>
-          <h3 className='text-violet-400'>Group Clickip Technologia</h3>
+        <div className='xl:min-w-96 xl:flex-col sm:leading-10'>
+          <h3 className='text-violet-400 '>Group Clickip Technologia</h3>
           <ul>
             <p>Click IP is a Internet provider holding</p>
             <li><p>• Developed React web app to avoid overloading the call center during Internet issues. (Vite, Zustand, Chakra UI). On average, we reduced support tickets by 35% during a network issue.</p></li>
@@ -30,14 +30,14 @@ export default function Experience() {
           </ul>
         </div>
       </section>
-      <section className='bg-zinc-900 rounded-2xl sm:flex-col sm:space-y-14 sm:mt-28 sm:mx-14 sm:leading-7 sm:py-14 sm:px-7'>
-        <div>
+      <section className='bg-zinc-900 rounded-2xl xl:text-2xl xl:flex xl:flex-row sm:flex-col sm:space-y-14 sm:mt-28 sm:mx-14 sm:leading-7 sm:py-14 sm:px-7'>
+        <div className='xl:min-w-96 xl:mt-14 xl:flex-col'>
           <h2 className='text-white font-bold'>IT Suport</h2>
           <span className='text-violet-400'>Employee</span>
           <p>time</p>
           <p>location</p>
         </div>
-        <div className='sm:leading-10'>
+        <div className='xl:min-w-96 xl:flex-col sm:leading-10'>
           <h3 className='text-violet-400'>Grupo Clickip Tecnologia</h3>
           <ul>
             <p>Click IP is a holding of Internet providers in Amazonas and Pará. Intlink, Wire, Fiber Network and Icom are part of this group</p>
@@ -48,14 +48,14 @@ export default function Experience() {
           </ul>
         </div>
       </section>
-      <section className='bg-zinc-900 rounded-2xl sm:flex-col sm:space-y-14 sm:mt-28 sm:mx-14 sm:leading-7 sm:py-14 sm:px-7'>
-        <div>
+      <section className='bg-zinc-900 rounded-2xl xl:text-2xl xl:flex xl:flex-row sm:flex-col sm:space-y-14 sm:mt-28 sm:mx-14 sm:leading-7 sm:py-14 sm:px-7'>
+        <div className='xl:min-w-96 xl:mt-14 xl:flex-col'>
           <h2 className='text-white font-bold'>It Intern</h2>
           <span className='text-violet-400'>Intern</span>
           <p>time</p>
           <p>location</p>
         </div>
-        <div className='sm:leading-10'>
+        <div className='xl:min-w-96 xl:flex-col sm:leading-10'>
           <h3 className='text-violet-400'>Tribunal Regional do Trabalho da 11ª Região</h3>
           <ul>
             <p>Main activities:</p>
